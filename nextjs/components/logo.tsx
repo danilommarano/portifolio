154048d2754e376df18342d668157e67b9482d04
Danilo Marano <danilo.m.marano@gmail.com>
2024-02-28 10:15:47 -0300

    feat: Developed Logo component

---
+++ nextjs/components/logo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+    return (
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-xl font-bold text-slate-200">danilo</span>
+        </a>
+    );
+}
